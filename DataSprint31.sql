006792fadfde7c1ebfac62fdd450c3a756a830a0
irmagabriels <60925653+irmagabriels@users.noreply.github.com>
2020-02-19 12:13:24 +0200

    Add files via upload

---
+++ DataSprint31.sql
@@ -0,0 +1,22 @@
+Labels	Catergories	Brands	Cost Price	Prices	Stock	Brands Sold for Month	Profit
+1	Chips	Simba 125g	8	16	50	45	384
+2	Chips	Lays 125g	9	18	50	50	450
+3	Chips	Doritos 125g	8	16	50	48	384
+4	Cooldrink	Coke 2lt	10	20	100	100	1000
+5	Cooldrink	Fanta 2lt	9	18	100	90	810
+6	Cooldrink	Sprite 2lt	9	18	100	98	882
+7	Chocolates	Cadbury 50g	5	10	50	48	192
+8	Chocolates	Tex 50g	5	10	50	50	250
+9	Chocolates	PS 50g	5	10	50	38	190
+10	Pies	Pepper Steak	10	20	40	39	390
+11	Pies	Chicken	8	16	40	30	240
+12	Pies	Steak & Kidney	10	20	40	40	400
+13	Fruit	Pear	6	12	50	45	270
+14	Fruit	Apple	6	12	50	40	240
+15	Fruit	Orange	5	10	50	39	195
+16	Cupcakes	Chocolate (6)	9	18	100	95	1710
+17	Cupcakes	Vanilla (6)	7	14	100	80	560
+18	Cupcakes	Blueberry (6)	10	20	100	100	1000
+19	Veg	Spinach	8	16	30	25	200
+20	Veg	Cabbage	8	16	30	30	240
+21	Veg	Tomato	5	10	30	30	150
