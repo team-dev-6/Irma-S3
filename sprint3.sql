30bf14cb443e48c0361aceeef0f441343cb0d097
irmagabriels <60925653+irmagabriels@users.noreply.github.com>
2020-02-20 10:16:35 +0200

    Add files via upload

---
+++ sprint3.sql
@@ -0,0 +1,23 @@
+Labels,Catergories,Brands,Cost Price,Prices,Stock,Brands Sold for Month,Profit,Barcode
+1,Chips,Simba 125g,8,16,50,45,384,1000456
+2,Chips,Lays 125g,9,18,50,50,450,1000457
+3,Chips,Doritos 125g,8,16,50,48,384,1000458
+4,Cooldrink,Coke 2lt,10,20,100,100,1000,1212133
+5,Cooldrink,Fanta 2lt,9,18,100,90,810,1212134
+6,Cooldrink,Sprite 2lt,9,18,100,98,882,1212135
+7,Chocolates,Cadbury 50g,5,10,50,48,192,9977881
+8,Chocolates,Tex 50g,5,10,50,50,250,9977882
+9,Chocolates,PS 50g,5,10,50,38,190,9977883
+10,Pies,Pepper Steak,10,20,40,39,390,4745152
+11,Pies,Chicken,8,16,40,30,240,4745153
+12,Pies,Steak & Kidney,10,20,40,40,400,4745154
+13,Fruit,Pear,6,12,50,45,270,1111119
+14,Fruit,Apple,6,12,50,40,240,1111118
+15,Fruit,Orange,5,10,50,39,195,1111117
+16,Cupcakes,Chocolate (6),9,18,100,95,1710,1238542
+17,Cupcakes,Vanilla (6),7,14,100,80,560,1238543
+18,Cupcakes,Blueberry (6),10,20,100,100,1000,1238544
+19,Veg,Spinach,8,16,30,25,200,7859423
+20,Veg,Cabbage,8,16,30,30,240,7859424
+21,Veg,Tomato,5,10,30,30,150,7859425
+22,Veg,Carrots,5,10,30,30,150,1501236
